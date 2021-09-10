1907374c63c950a628cfda0822f5e901cd0663dc
Everton Barretto <evertonbarreto@aformulabr.com.br>
2021-09-10 12:31:01 -0300

    b

--- src/LaravelModulosServiceProvider.php
+++ src/LaravelModulosServiceProvider.php
@@ -1,18 +1,15 @@
 <?php
 
 namespace evertonbarreto\laravel_modulos;
-use App\Console\Commands\Controller;
-use App\Console\Commands\Model;
-use App\Console\Commands\Modulo;
+
 use Illuminate\Support\ServiceProvider;
+use src\Commands\Modulo;
 
 class LaravelModulosServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->commands([
-            Controller::class,
-            Model::class,
             Modulo::class
         ]);
     }
