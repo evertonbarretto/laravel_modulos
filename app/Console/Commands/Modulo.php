3f93fad327b71a67a3da1cf35b947ae8780db2a7
Everton Barretto <evertonbarreto@aformulabr.com.br>
2021-09-10 12:10:03 -0300

    comandos

---
+++ app/Console/Commands/Modulo.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Modulo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'modulo:make {nome}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $raiz = 'Modulos/'.$this->argument('nome').'/';
+        $pastas[] = $raiz.'FiltersMaps';
+        $pastas[] = $raiz.'Http';
+        $pastas[] = $raiz.'Http/Controllers';
+        $pastas[] = $raiz.'database';
+        $pastas[] = $raiz.'database/migrations';
+        $pastas[] = $raiz.'database/factories';
+        $pastas[] = $raiz.'Models';
+        $pastas[] = $raiz.'Providers';
+        $pastas[] = $raiz.'Repositories';
+        $pastas[] = $raiz.'routes';
+        $pastas[] = $raiz.'Services';
+        $pastas[] = $raiz.'Tests';
+
+
+        mkdir($raiz);
+        foreach ($pastas as $pasta)
+        {
+            mkdir($pasta);
+        }
+        $this->createFile('Modulos//'.$this->argument('nome').'//routes//web.php');
+
+        return 1;
+    }
+
+    private function createFile($path)
+    {
+
+        $myfile = fopen($path, "w") or die("Unable to open file!");
+        $txt = "<?php\n";
+        fwrite($myfile, $txt);
+        $txt = "use Illuminate\Support\Facades\Route;\n";
+        fwrite($myfile, $txt);
+        fclose($myfile);
+    }
+}
